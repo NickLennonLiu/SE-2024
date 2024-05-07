3e2235df754bc965681a481a5b13b7fcb2e8f546
river <junetheriver@gmail.com>
2024-05-07 10:32:53 +0800

    build

--- dist/assets/slidev/default-35ADp9k9.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-BjgqcxGK.js";import{_ as l}from"../index-Bzc4zXaw.js";const c={},d={class:"slidev-layout default"};function n(e,r){return o(),t("div",d,[s(e.$slots,"default")])}const i=l(c,[["render",n],["__file","/opt/homebrew/lib/node_modules/@slidev/cli/node_modules/@slidev/client/layouts/default.vue"]]);export{i as I};
